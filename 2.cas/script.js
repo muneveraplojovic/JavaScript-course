1d5e226347d70f1d68b40fda7cfc8dcdeab65b76
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-15 23:03:17 +0100

    2 cas

--- 2.cas/script.js
+++ 2.cas/script.js
@@ -82,6 +82,8 @@
 // 3. povecanjeBrzine: (metoda koja uzima jedan argument),
 // 4. smanjenjeBrzine: (metoda koja uzima jedan argument),
 
+
+
 const myCar = {
     id: 1,
     marka: "Audi",
@@ -99,21 +101,90 @@ const myCar = {
     maxBrzina: 260,
     trBrzina: 200,
   
-    drive: function (brzina) {
-      if (this.trBrzina + brzina > this.maxBrzina) {
-        return "Ne moze, kupi bolji auto :((";
-      } else {
-        return `Trenutna brzinina iznosi: ${this.trBrzina+=80}`;
-      }
-    },
+    // drive: function (brzina) {
+    //   if (this.trBrzina + brzina > this.maxBrzina) {
+    //     return "Ne moze, kupi bolji auto :((";
+    //   } else {
+    //     return `Trenutna brzinina iznosi: ${this.trBrzina+=80}`;
+    //   }
+    // },
   
-    brake: function (brzina) {
-      return (`${this.trBrzina -= brzina} koooociii`);
-    },
+    // brake: function (brzina) {
+    //   return (`${this.trBrzina -= brzina} koooociii`);
+    // },
   
-    stop: function () {
-      return `${(this.trBrzina = 0)} trenutna brzina`;
-    },
+    // stop: function () {
+    //   return `${(this.trBrzina = 0)} trenutna brzina`;
+    // },
+
+
+  //brisemo putem delete metode
+  //
+
   };
   
   console.log(myCar.drive(80));
+
+  //--------------------------------------------------------------
+
+  // Domaci zadatak:
+// Unutar postojeceg objekta dodati nova svojstva:
+// 1. trenutnaBrzina: 0,
+// 2. maksimalnaBrzina: 260,
+// 3. povecanjeBrzine: (metoda koja uzima jedan argument),
+// 4. smanjenjeBrzine: (metoda koja uzima jedan argument),
+
+const myCar = {
+  id: 1,
+  marka: "Audi",
+  model: "a4",
+  boja: "Crvena",
+  pogon: "prednji",
+  menjac: "automatski",
+  kontakt: [0622222, 02033322],
+  servis: {
+    datum: "04,maj",
+    km: 23000,
+    serviser: "Pasovic",
+  },
+  udaran: true,
+};
+
+console.log(myCar["menjac"]);
+console.log(myCar.servis["datum"]);
+console.log(myCar.servis.datum);
+console.log(myCar["servis"]["datum"]);
+
+myCar.trenutnaBrzina = 0;
+myCar.maksimalnaBrzina = 260;
+myCar.povecanjeBrzine = function (brzina) {
+  if (this.trenutnaBrzina + brzina > this.maksimalnaBrzina) {
+    console.log(`Nije moguce voziti vise od maksimalne brzine.`);
+  } else if (brzina < 0 || isNaN(brzina)) {
+    console.log(`Niste uneli adekvatno povecanje brzine.`);
+  } else {
+    this.trenutnaBrzina += brzina;
+  }
+};
+
+myCar.smanjenjeBrzine = function (brzina) {
+  if (this.trenutnaBrzina - brzina < 0) {
+    console.log(`Nije moguce voziti sporije od 0 Kmh.`);
+  } else if (brzina < 0 || isNaN(brzina)) {
+    console.log(`Niste uneli adekvatno smanjenje brzine.`);
+  } else {
+    this.trenutnaBrzina -= brzina;
+  }
+};
+
+myCar.ukoci = function () {
+  this.trenutnaBrzina = 0;
+};
+// console.log(myCar);
+myCar.povecanjeBrzine(100);
+myCar.smanjenjeBrzine(70);
+// myCar.smanjenjeBrzine(60);
+myCar.ukoci();
+
+console.log(myCar.trenutnaBrzina);
+console.log(myCar);
