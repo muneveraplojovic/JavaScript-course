f0084f9297e689b3cdbff9bd53ddffc412f5e1bf
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-27 23:52:58 +0100

    spread operator

--- 7.cas/script2.js
+++ 7.cas/script2.js
@@ -14,29 +14,29 @@
 
 // ...spread operator nam sluzi za pravljenje kopije niza ili objekta, kao i spajanje vise nizova (objekata).
 
-const niz1 = ["sto", "stolica", "tabla", "marker"];
-const niz2 = [...niz1];
-console.log(niz2);
-niz2[1] = "fotelja";
-console.log(niz2);
-console.log(niz1);
-// Spajanje vise nizova
-const niz3 = [...niz, ...niz1];
-console.log(niz3);
-const niz4 = [15, ...niz2, 150];
-console.log(niz4);
+// const niz1 = ["sto", "stolica", "tabla", "marker"];
+// const niz2 = [...niz1];
+// console.log(niz2);
+// niz2[1] = "fotelja";
+// console.log(niz2);
+// console.log(niz1);
+// // Spajanje vise nizova
+// const niz3 = [...niz, ...niz1];
+// console.log(niz3);
+// const niz4 = [15, ...niz2, 150];
+// console.log(niz4);
 
-const obj = {
-  ime: "Muhamed",
-  prezime: "Krkmisevic",
-  starost: 17,
-};
-const newObj = {
-  ...obj,
-  bojaKose: "crna",
-  kovrdzav: false,
-};
-console.log(newObj);
+// const obj = {
+//   ime: "Muhamed",
+//   prezime: "Krkmisevic",
+//   starost: 17,
+// };
+// const newObj = {
+//   ...obj,
+//   bojaKose: "crna",
+//   kovrdzav: false,
+// };
+// console.log(newObj);
 
 // Napraviti funkciju koja vraca novi niz, gde je svaki element objekat sa vrednostima title i budget.
 const movies = [
