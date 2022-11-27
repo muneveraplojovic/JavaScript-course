4378a811e935ccccfd9254fa005debfbcc4e02e6
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-27 23:54:46 +0100

    square list function

--- 7.cas/script.js
+++ 7.cas/script.js
@@ -61,24 +61,24 @@
 //  (decimal numbers are not integers) when an array of real numbers is passed to it.
 //   An example of an array of real numbers is [-3, 4.8, 5, 3, -3.2].
 
-const niz = [-3, 4.8, 5, 3, -3.2];
-const squareList = (arr) => {
-  const filtered = arr.filter(function (number) {
-    return number > 0 && number % 1 === 0;
-  });
-  const squares = filtered.map(function (number) {
-    return number ** 2;
-  });
-  return squares;
-};
+// const niz = [-3, 4.8, 5, 3, -3.2];
+// const squareList = (arr) => {
+//   const filtered = arr.filter(function (number) {
+//     return number > 0 && number % 1 === 0;
+//   });
+//   const squares = filtered.map(function (number) {
+//     return number ** 2;
+//   });
+//   return squares;
+// };
 
-console.log(squareList(niz));
-const a = 14;
-const b = 14.15;
-// console.log(a % 1 === 0);
-// console.log(b % 1 === 0);
+// console.log(squareList(niz));
+// const a = 14;
+// const b = 14.15;
+// // console.log(a % 1 === 0);
+// // console.log(b % 1 === 0);
 
-// Number.isInteger() metoda varaca boolean u zavisnosti od toga da li je argument date metode ceo broj ili nije.
+// // Number.isInteger() metoda varaca boolean u zavisnosti od toga da li je argument date metode ceo broj ili nije.
 
-console.log(Number.isInteger(a));
-console.log(Number.isInteger(b));
+// console.log(Number.isInteger(a));
+// console.log(Number.isInteger(b));
