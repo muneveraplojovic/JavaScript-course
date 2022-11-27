1e77c8dee21634a86d1f82fd5713f38255a1cf81
Munevera Plojovic <plojovicm3@gmail.com>
2022-11-27 23:52:13 +0100

    erase duplicates

--- 7.cas/script2.js
+++ 7.cas/script2.js
@@ -1,16 +1,16 @@
 // Napraviti funkciju koja nam vraca novi niz koji nema duplikata:
 
-const niz = [12, 1, 3, 2, 3, 4, 6, 4];
-const eraseDuplicate = function (arr) {
-  const newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!newArr.includes(arr[i])) {
-      newArr.push(arr[i]);
-    }
-  }
-  return newArr;
-};
-console.log(eraseDuplicate(niz));
+// const niz = [12, 1, 3, 2, 3, 4, 6, 4];
+// const eraseDuplicate = function (arr) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!newArr.includes(arr[i])) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// };
+// console.log(eraseDuplicate(niz));
 
 // ...spread operator nam sluzi za pravljenje kopije niza ili objekta, kao i spajanje vise nizova (objekata).
 
