f27f4a99b6439ab6148615d0e163d5721c8c14cb
Munevera Plojovic <plojovicm3@gmail.com>
2022-12-16 20:46:12 +0100

    sets, maps, scope

---
+++ 14.cas/script.js
@@ -0,0 +1,28 @@
+// function myFunc() {
+//   let broj = 4;
+//   let predmet = "stolice";
+//   return `U prostoru imamo ${broj} ${predmet}.`;
+// }
+
+// console.log(myFunc());
+//local scope
+//Poznata je cinjenica da kada se izvrsi funkcija, sve njene lokalne promenljive pokupi "garbage" kolektor i one prestaju dda postoje u memoriji.
+//To nije slucaj ako postoji funkcija koja zahteva koriscenje promenljvih iz domena druge funkcije
+//To se najcesce postize ugradjivanjem funkcije unutar druge funkcije
+
+// function myFunc() {
+//     let broj = 4;
+//     let predmet = "stolice";
+//     function secondFunc(arg) {
+//         console.log(arg + broj);
+//     }
+//     secondFunc(10);
+//     return `To je bio primer closure-a u js-u`;
+// }
+
+// console.log(myFunc());
+
+//clousure je funkcija koja zahteva koriscenje promenljivih iz domena druge funkcije.
+
+
+
---
+++ 14.cas/script2.js
@@ -0,0 +1,39 @@
+//setovi i
+
+//setovi predstavljaju kolekciju unique values
+//svaki se element se pojavljuje samo jednom
+
+//Kreiranje setova:
+
+const letters = new Set(["a", "b", "c", "d", "a"]);
+//zadnje a se ne ispusuje jer ne prima duplikate, moze da se doda
+console.log(letters);
+
+//Metode za setove:
+//add( ) - dodaje novi element u setu
+letters.add("m");
+console.log(letters);
+//delete() - brise specifican element u sebi
+letters.delete("m");
+console.log(letters);
+//has() - vraca boolean - da li se neka vrednost nalazi u setu
+console.log(letters.has("m"));
+//forEach() - poziva callback funkciju za svaki element
+letters.forEach((el) => {
+  console.log(el);
+});
+//values()
+console.log(letters.values());
+console.log("----------------------------------");
+//Napraviti funckiju koja iz datog niza vraca novi niz brisuci duplikate originalnog
+
+const array = [1, 2, 3, 4, 5, 1, 2, 3, 4, 6, 1, 4];
+
+function getNewArray(arg) {
+  let newArray = new Set(arg);
+  return [...newArray];
+}
+console.log(getNewArray(array));
+
+
+
