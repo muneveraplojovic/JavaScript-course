691a29db59fc28dfbcd280cac02b8b419a3f1221
Munevera Plojovic <plojovicm3@gmail.com>
2022-12-20 11:35:14 +0100

    async js

---
+++ 16.cas/script.js
@@ -0,0 +1,16 @@
+//closure, scope;
+//sklanjanje nekih dugih funk na web api, asinhrone strane izbacujemo iz v8 i 
+//saljemo u web api i onda u queue, event lopp i onda u stack
+
+//last in first up
+//event loop, queue, web api
+//web api, redosledom mojim su se zavrsile ubacuju se u queue
+//mehanizmi za async js
+//callback, promises async await
+//funk referentni tip podatka koji mozemo da prosledjujemo u raznim oblicima
+//callback
+//callback se slabo koristi ja ga hadalj ucila...
+//promises-sl
+//chat.openapi.com
+//setTimeOut()
+
