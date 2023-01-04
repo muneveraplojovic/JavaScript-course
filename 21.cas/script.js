8be1d9634a128f778d267849bc8df2d8057ccfbc
Munevera Plojovic <plojovicm3@gmail.com>
2023-01-04 23:58:21 +0100

    debugging...

---
+++ 21.cas/script.js
@@ -0,0 +1,14 @@
+// class Car {
+//   constructor(carName, carType) {
+//     this.name = carName;
+//     this.type = carType;
+//   }
+//   showAttr() {
+//     console.log(this.name, this.type);
+//   }
+// }
+
+// const newCar = new Car("audi", "limo");
+// newCar.showAttr();
+
+
